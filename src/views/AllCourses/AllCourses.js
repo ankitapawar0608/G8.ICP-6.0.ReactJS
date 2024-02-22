ed3824abca85842547685514b2bea89c029db351
vansh bharne <114802824+vanshbharne@users.noreply.github.com>
2024-02-22 16:47:57 +0530

    done

--- src/views/AllCourses/AllCourses.js
+++ src/views/AllCourses/AllCourses.js
@@ -1,8 +1,20 @@
 import React from 'react'
-
+import "./AllCourses.css"
+import { info } from '../../components/AllCourcesContent/content'
+import ALLCources from '../../components/AllCourcesCard/ALLCourcesCard'
 function AllCourses() {
   return (
-    <div>AllCourses</div>
+    <div>
+      
+      <div className='card-container my-5'> 
+      {
+        info.map((info,index)=>{
+        
+          return(<ALLCources key={index} info={info} />)
+        })
+      }
+      </div>
+    </div>
   )
 }
 
