1c7b4b071a7ee392a9763e48ff6af5c92824a2f0
ankitapawar0608 <kedarpawar775@gmail.com>
2024-02-22 15:53:53 +0530

    Navbar added in all pages

--- src/views/AboutUs/AboutUs.js
+++ src/views/AboutUs/AboutUs.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 
 function AboutUs() {
   return (
+    <>
+    <Navbar/>
     <div>AboutUs</div>
+    </>
   )
 }
 
--- src/views/AllCourses/AllCourses.js
+++ src/views/AllCourses/AllCourses.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
 
 function AllCourses() {
   return (
+    <>
+    <Navbar/>
     <div>AllCourses</div>
+    </>
   )
 }
 
