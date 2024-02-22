c9ebd979127a9f5a2f193116c99bc1ac2d705cbc
Shweta Vasekar <shwetasvasekar@gmail.com>
2024-02-22 16:52:22 +0530

    Aboutuspage

--- src/views/AboutUs/AboutUs.js
+++ src/views/AboutUs/AboutUs.js
@@ -1,9 +1,57 @@
-import React from 'react'
-
-function AboutUs() {
+import React from 'react';
+import './AboutUs.css'; // Import CSS file for styling
+import shweta from '../AboutUs/shweta.png';
+import anika from '../AboutUs/anika.png';
+import vansh from '../AboutUs/vansh.png';
+const AboutUs = () => {
   return (
-    <div>AboutUs</div>
-  )
-}
+    <div>
+      <h1 style={{paddingLeft:'550px'}}>AboutUs</h1>
+    <div className="about-container" style={{widows:"1000px"}}>
+      {/* Big container with blue border shadow */}
+      <div className="big-container">
+        {/* Three different sections */}
+        <div className="section">
+          {/* First block */}
+          <div className="block">
+            <img src={anika}></img>
+            <div className="info">
+              <h2>Ankita Pawar</h2>
+              <p>Teaching Assistant</p>
+              <a href="mailto:your.email@gmail.com" className="button">Gmail</a>
+      <a href="https://www.linkedin.com/in/yourprofile" className="button">LinkedIn</a>
+            </div>
+          </div>
+          <hr className="line" />
+          {/* Second block */}
+          <div className="block">
+            <img src={shweta}></img>
+            <div className="info">
+              <h2>Shweta Vasekar</h2>
+              <p>Instructor</p>
+              <a href="mailto:your.email@gmail.com" className="button">Gmail</a>
+      <a href="https://www.linkedin.com/in/yourprofile" className="button">LinkedIn</a>
+            </div>
+          </div>
+          <hr className="line" />
+          {/* Third block */}
+          <div className="block">
+            <img src={vansh}></img>
+            <div className="info">
+              <h2>Vansh Bharne</h2>
+              <p>Teaching Assistant</p>
+              <a href="mailto:your.email@gmail.com" className="button"> Gmail</a>
+      <a href="https://www.linkedin.com/in/yourprofile" className="button">LinkedIn</a>
+            </div>
+          </div>
+        </div>
+        {/* Line between each block */}
+        <hr className="line" />
+        <hr className="line" />
+      </div>
+    </div>
+    </div>
+  );
+};
 
-export default AboutUs
+export default AboutUs;
