63234c5272bdd596aa83dd94fd6318f98a97c385
ankitapawar0608 <kedarpawar775@gmail.com>
2024-02-23 11:52:06 +0530

    New section

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,12 +9,12 @@ function Footer() {
         <footer className="footer">
             <div className="container">
                 <div className="row">
-                    <div className="col-md-3">
+                    <div className="col-lg-3 col-md-6 col-sm-12">
                         <h4>CogNexLearn</h4>
                         <p>We are India's Most Loved Coding Community. Join us! 
                         </p>
                     </div>
-                    <div className="col-md-3">
+                    <div className=" col-lg-3 col-md-6 col-sm-12">
                         <h5>Helpful Links</h5>
                         <ul className="list-unstyled">
                             <li><Link to="/">Home</Link></li>
@@ -25,7 +25,7 @@ function Footer() {
                             <li><Link to="/login">Login</Link></li>
                         </ul>
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-lg-3 col-md-6 col-sm-12">
                         <h5>Learn</h5>
                         <ul className="list-unstyled">
                             <li><Link to="/buycourses">Fullstack Development</Link></li>
@@ -34,7 +34,7 @@ function Footer() {
                             <li><Link to="/buycourses">C++ Programming</Link></li>
                         </ul>
                     </div>
-                    <div className="col-md-3">
+                    <div className="col-lg-3 col-md-6 col-sm-12">
                         <p>Contact us</p>
                         <p><Link to="tel:+918830779433">+91-8888888888</Link></p>
                         <p><a href="mailto:ankitapawar0608@gmail.com">cognexlearn@gmail.com</a></p>
